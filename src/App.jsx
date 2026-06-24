b8717374efb26add52ff7f34eb127462a91be50d
raj <a.r9111379171@gmail.com>
2026-06-24 08:08:07 +0530

    Fixed leaflet default marker icon path

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ import {io} from 'socket.io-client'
 import {MapContainer,TileLayer,Marker,Popup,useMap} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
+import L from 'leaflet';
+
+// Leaflet ke default broken icon paths ko theek karne ke liye code
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
+
 
 // Backend server se connect kiya. Jaise hi ye line chalti hai, 
 // Socket.io piche se apne aap backend par "connection" event bhej deta hai!
